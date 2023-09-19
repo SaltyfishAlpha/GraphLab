f06b227437156b565e3f537b1e6d87eb8adf9fb9
SaltyfishAlpha <saltyfishalpha@gmail.com>
2023-09-19 17:07:23 +0800

    setup

---
+++ 3rdparty/glad.cmake
@@ -0,0 +1,4 @@
+add_library(glad ${CMAKE_CURRENT_SOURCE_DIR}/glad/src/glad.c)
+target_include_directories(glad PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/glad/include)
+
+target_link_libraries(glfw ${GL_LIBRARY} glad)
